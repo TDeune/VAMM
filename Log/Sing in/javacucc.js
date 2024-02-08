d40a954841487591858f859409afd8fdebd37d21
akosv <akos.vanger1@gmail.com>
2024-02-08 10:07:17 +0100

    asdd

--- Log/Sing in/javacucc.js
+++ Log/Sing in/javacucc.js
@@ -2,23 +2,13 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 window.onclick = function(event) {
+    var dropdowns;
+    var i;
+    var openDropdown;
     if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
+        dropdowns = document.getElementsByClassName("dropdown-content");
         for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn2')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
+            openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
             }
